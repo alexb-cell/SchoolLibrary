8e5bcc6f92a9b6e0de75e0d5549964e9c0dafa1e
alexshikma <alexshikma@gmail.com>
2025-09-26 12:50:05 +0300

    Add project files.

---
+++ LibraryModels/Author.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModels
+{
+    public class Author
+    {
+        string authorId;
+        string authorFirstName;
+        string authorLastName;
+        int authorYear;
+        int countryId;
+        string authorPicture;
+
+
+        public string AuthorId
+        {
+            get { return this.authorId; }
+            set { this.authorId = value; }
+        }
+        [Required(ErrorMessage = "Author First Name cannot be empty")]
+        [StringLength(20,MinimumLength =2,ErrorMessage = " A firs name cannot be less than two characters or more than twenty characters.")]
+        [FirstLetterCapital(ErrorMessage = "The first letter of the name must be a capital letter and the rest must be lowercase.")]
+        public string AuthorFirstName
+        {
+            get { return this.authorFirstName; }
+            set { this.authorFirstName = value; }
+        }
+
+        [Required(ErrorMessage = "Author Last Name cannot be empty")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = " A last name cannot be less than two characters or more than twenty characters.")]
+        [FirstLetterCapital(ErrorMessage = "The first letter of the name must be a capital letter and the rest must be lowercase.")]
+        public string AuthorLastName
+        {
+            get { return this.authorLastName; }
+            set { this.authorLastName = value; }
+        }
+
+
+        public  int AuthorYear
+        {
+            get { return this.authorYear; ; }
+            set { this.authorYear = value; }
+        }
+        public int CountryId
+        {
+            get { return this.countryId; ; }
+            set { this.countryId = value; }
+        }
+
+        public string AuthorPicture
+        {
+            get { return this.authorPicture; }
+            set { this.authorPicture = value; }
+        }
+
+    }
+}
---
+++ LibraryModels/CatalogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModels
+{
+    public class CatalogViewModel
+    {
+        public List<Author> Authors { get; set; }
+        public List<Book> Books { get; set; }
+        public List<Ganre> Ganres { get; set; }
+    }
+}
